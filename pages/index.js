c0f78f28ccf4b3b4ef3057dec3261376a5ca647c
ShintaroaSuzuki <50007328+ShintaroaSuzuki@users.noreply.github.com>
2020-12-13 16:14:49 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,72 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>toqetwarten株式会社</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <div className="gradient">
+                    <img src="/gorilla_smooth.jpeg" />
+                    <div className="titleLogo">
+                        <p className="title">toqetwarten</p>
+                        <hr className="logoBar" />
+                    </div>
+                    <div className="homeTabs">
+                        <Link href="./about">
+                            <a className="tab">about</a>
+                        </Link>
+                        <Link href="./products">
+                            <a className="tab">products</a>
+                        </Link>
+                        <Link href="./IR">
+                            <a className="tab">IR</a>
+                        </Link>
+                        <Link href="./recruit">
+                            <a className="tab">recruit</a>
+                        </Link>
+                        <Link href="./contact">
+                            <a className="tab">contact</a>
+                        </Link>
+                    </div>
+                    <p className="message">繊細で、逞しい。</p>
+                </div>
+            </main>
+            <main>
+                <h1 className="heading1">目指すは、日本のシルバーバック。</h1>
+                <p className="explain">
+                    シルバーバックをご存知でしょうか。実は、ゴリラの中には背中の毛が白いゴリラと、そうでないゴリラがいるのです。
+                </p>
+                <p className="explain">
+                    この背中の白いゴリラをシルバーバックといい、シルバーバックは群れの長を務めています。
+                    <br />
+                    シルバーバックが亡くなると、また新たなシルバーバックが群れの中から生まれます。
+                </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <p className="explain">
+                    私たちは、日本という「群れ」の中の、新たなシルバーバックになり、日本を先導する会社でありたいと思い、日々活動しています。
+                </p>
+            </main>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <img
+                        src="/vercel.svg"
+                        alt="Vercel Logo"
+                        className={styles.logo}
+                    />
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    );
 }
